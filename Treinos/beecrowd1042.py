989eb5b2c7d861ed9ddc98fc1c012c95439ebebe
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2023-12-26 16:44:07 -0300

    Create beecrowd1042.py
    Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.

---
+++ Treinos/beecrowd1042.py
@@ -0,0 +1,20 @@
+#Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+ordenados = sorted([n1,n2,n3])
+
+print(ordenados[0])
+print(ordenados[1])
+print(ordenados[2])
+
+
+print()
+
+print(n1)
+print(n2)
+print(n3)
+
+
