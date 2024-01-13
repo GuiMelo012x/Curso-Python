0926f5325b4a99ec32ae60a06b49edf0045c882f
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-13 15:17:11 -0300

    Desafio 11 - Contando com For Loops

---
+++ Desafios/desafio11.py
@@ -0,0 +1,19 @@
+# A117 - Desafio 11 - Contando com For Loops
+'''
+
+Use a função range() em um for loop para imprimir os números de 1 a 10 na tela.
+
+'''
+
+for i in range(1,11):
+    print(i)
+
+print('-----')
+
+for i2 in range(11):
+    print(i2)
+
+print('-----')
+
+for i3 in range(1,11,2): # (start,stop,step) -- > o step iria de 2 em 2, neste caso e printaria 1,3,5,7,9.
+    print(i3)
