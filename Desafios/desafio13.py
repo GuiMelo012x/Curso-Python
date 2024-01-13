e0e175192d641374345abe34d6fa90c184b41057
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-13 15:17:57 -0300

    Desafio 13 - While Loop

---
+++ Desafios/desafio13.py
@@ -0,0 +1,16 @@
+# A121 - Desafio 13 - While Loop
+'''
+
+Utilize um while para imprimir os números de 1 a 10
+
+'''
+
+i = int(input('É para contar até quanto? '))
+def contar(i):
+    j = 0
+    while j < i:
+        j = j + 1
+        print(j)
+
+
+contar(i)
