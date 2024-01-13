ecd154e131cfa794ab65b3ebf420ce33211131ee
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-13 14:55:50 -0300

    Desafio 10 - Listas com Loops

---
+++ Desafios/desafio10.py
@@ -0,0 +1,12 @@
+# A115 - Desafio 10 - Listas com Loops
+'''
+
+  Utilizando a lista "frutas", use um for loop para percorrer a lista e imprima cada fruta 
+  precedida pelo texto "Eu gosto de".
+
+'''
+
+frutas = ['maçã','morango','uva']
+
+for i in frutas:
+    print('Eu gosto de',i)
