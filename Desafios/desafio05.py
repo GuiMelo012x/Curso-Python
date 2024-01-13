d0cfed06697a24460b788e37edc699dc34a068c2
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-13 14:42:26 -0300

    Desafio 5 - Cálculos Matemáticos

---
+++ Desafios/desafio05.py
@@ -0,0 +1,34 @@
+# A105 - Desafio 5 - Cálculos Matemáticos.
+'''
+
+Crie um script que solicite ao usuário dois números. Em seguida ,seu script deve imprimir a soma,
+subtração, multiplicação, divisão, e a exponenciação desses dois números.
+
+'''
+num1 = float(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: '))
+print()
+def soma(num1,num2):
+    return print('Soma:',num1 + num2)
+
+def subtracao (num1,num2):
+    return print('Subtração:', num1-num2)
+
+
+def multiplicacao(num1,num2):
+    return print('Multiplicação:', num1*num2)
+
+def divisao(num1,num2):
+    return print('Divisão:', num1/num2)
+
+def exponenciacao(num1,num2): # ** = elevado
+    return print('Exponenciação:',num1**num2)
+
+
+soma(num1,num2)
+subtracao(num1,num2)
+multiplicacao(num1,num2)
+divisao(num1,num2)
+exponenciacao(num1,num2)
+
+
