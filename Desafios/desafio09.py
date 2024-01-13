a6702c461118bdba0a1850f3e87e62ca244ee9dc
Guilherme Melo <112731659+GuiMelo012x@users.noreply.github.com>
2024-01-13 14:53:08 -0300

    Desafio 9 - Removendo itens da Lista

---
+++ Desafios/desafio09.py
@@ -0,0 +1,16 @@
+# A113 - Desafio 9 - Removendo itens da Lista
+'''
+
+Utilizando a lista "frutas", remova a "manga" da lista utilizando o método remove().
+Depois, remova o último item da lista usando o comando del, e depois imprima.
+
+'''
+
+frutas = ['maçã','morango','manga','uva','abacaxi']
+
+frutas.remove('manga') # remove a manga
+frutas.__delitem__(3) # remove o abacaxi
+del frutas [0] # remove a maçã
+
+for i in frutas:
+    print(i)
